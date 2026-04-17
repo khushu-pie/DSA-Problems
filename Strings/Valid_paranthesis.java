57bc18ed8000c5a5dc6f6d70fccc0ce8bd366924
Khushi <khushinawal2005@gmail.com>
2026-04-17 19:08:47 +0530

    Implement valid parentheses checker using stack

---
+++ Strings/Valid_paranthesis.java
@@ -0,0 +1,24 @@
+import java.util.Stack;
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st=new Stack<>();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='(' || s.charAt(i)=='{' || s.charAt(i)=='['){
+                st.push(s.charAt(i));
+            } else{
+                if(st.size()==0){//corner case when closing brackets are greater than opening brackets
+                    return false;
+                }
+                if((s.charAt(i)==')' && st.peek()=='(')||(s.charAt(i)=='}' && st.peek()=='{')|| (s.charAt(i)==']' && st.peek()=='[')){
+                    st.pop();
+                } else{
+                    return false;
+                }
+            }
+        }
+        return st.size()==0;//corner case when opening brackets are greater than closing brackets
+            
+        
+    }
+    
+}
